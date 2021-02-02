8c7d6aa063f5abad210a0562ddb5a2064bd51d22
Jones Ogolo <jay.topher11@gmail.com>
2021-02-02 23:40:46 +0100

    Creaded a register route

---
+++ src/routes/authRouter.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import AuthController from '../controllers/auth';
+
+const router = Router();
+
+/** Register user route */
+router.post('/', async function (req, res) {
+  try {
+    const { email, username, password, first_name, last_name } = req.body;
+    const data = await AuthController.registerUser({
+      email,
+      username,
+      password,
+      first_name,
+      last_name,
+    });
+    res.status(201).json({ data });
+    return;
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
