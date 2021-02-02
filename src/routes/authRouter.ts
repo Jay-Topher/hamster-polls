631df224e5153e5d104dc26f55ad256379782d22
Jones Ogolo <jay.topher11@gmail.com>
2021-02-02 23:42:17 +0100

    added doc

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,7 +3,9 @@ import AuthController from '../controllers/auth';
 
 const router = Router();
 
-/** Register user route */
+/** Register user route
+ * payload { email, password, first_name, last_name, username }
+ */
 router.post('/', async function (req, res) {
   try {
     const { email, username, password, first_name, last_name } = req.body;
