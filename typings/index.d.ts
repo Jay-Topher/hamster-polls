2f0582eb81f75ebcb11e9868b57975e3735bf1fe
Jones Ogolo <jay.topher11@gmail.com>
2021-02-02 22:08:53 +0100

    created default type file

---
+++ typings/index.d.ts
@@ -0,0 +1,13 @@
+interface JwtPayload {
+  id: string;
+  username: string;
+  email: string;
+}
+
+interface UserType {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  username: string;
+}
