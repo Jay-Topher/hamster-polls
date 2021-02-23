db2b8d20a6ae3f8b75ecc6c2ee9f4e4325a08244
Jones Ogolo <jay.topher11@gmail.com>
2021-02-23 13:28:32 +0100

    created a controller fn to publish polls

--- src/controllers/poll/index.ts
+++ src/controllers/poll/index.ts
@@ -27,3 +27,35 @@ async function addPoll({ title, description = '', author_id }: PollType) {
   }
 }
 
+/**
+ * A Controller Fn to publish a poll
+ * @param pollId The ID of the poll to publish
+ */
+async function publishPoll(pollId: string) {
+  try {
+    const [publishedPoll] = await sql<
+      PollTable
+    >`UPDATE polls SET published = ${true} WHERE id = ${pollId} RETURNING *`;
+
+    if (!publishPoll) {
+      throw new APIError({
+        status: 404,
+        message: 'Poll to publish not found',
+        errors: 'Poll to publish not found',
+      });
+    }
+
+    return publishedPoll;
+  } catch (error) {
+    throw new APIError({
+      errors: error,
+      message: error.message || error,
+      status: 500,
+    });
+  }
+}
+
+export default {
+  addPoll,
+  publishPoll,
+};
