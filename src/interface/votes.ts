55e918ffa02b3b1daaf2f088370f838a405d1e0a
Jones Ogolo <jay.topher11@gmail.com>
2021-02-26 23:08:49 +0100

    added type to interface

--- src/interface/votes.ts
+++ src/interface/votes.ts
@@ -1,6 +1,7 @@
 import TableCommons, { TimeStamp } from './interfaceCommons';
 
 export default interface VotesTable extends TableCommons {
+  question_id: string;
   option_id: string;
   voter_id: string;
   voted_at: TimeStamp;
