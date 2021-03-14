91ab19235e0111d59ba53a9ce215e3a71f1095c2
Jones Ogolo <jay.topher11@gmail.com>
2021-03-14 14:51:55 +0100

    added query

--- src/schema.ts
+++ src/schema.ts
@@ -1,22 +1,8 @@
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-
-import sampleController from './controllers/sample';
-import SampleType from './types/sample';
-
-const query = new GraphQLObjectType({
-  name: 'Query',
-  description: 'The query root of Nert.',
-  fields: () => ({
-    sample: {
-      type: SampleType,
-      description: 'A sample root schema',
-      resolve: () => sampleController(),
-    },
-  }),
-});
+import { GraphQLSchema } from 'graphql';
+import { RootQueryType } from './types';
 
 const schema = new GraphQLSchema({
-  query,
+  query: RootQueryType,
 });
 
 export default schema;
